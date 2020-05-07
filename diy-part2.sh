30a1b9570ace0dd6b2e9d663a104fa2a15d4944b
Lsun01 <xroot009@icloud.com>
2020-05-07 16:47:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,6 +7,15 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #============================================================
-
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+git clone https://github.com/vernesong/OpenClash package/luci-app-OpenClash
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/Lienol/openwrt-package package/openwrt-package
+#now dir is openwrt
+mkdir package/base-files/files/config
+echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+./scripts/feeds update -a
+./scripts/feeds install -a
